670ac93652307cd278b377755cf3b3482409b997
Velenir <velenir@gmail.com>
2020-11-22 15:36:43 +0300

    don't bundle node_modules

--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -1,7 +1,9 @@
 const merge = require('webpack-merge');
+const nodeExternals = require('webpack-node-externals');
 
 const common = require('./webpack.common.config.js');
 
 module.exports = merge(common(), {
-  mode: 'production'
+  mode: 'production',
+  externals: [nodeExternals()],
 });
